46b03135b4d16cf655f94ce0c333465e52371424
luiza08 <Luiza08>
2022-01-26 21:15:37 +0600

    first commit

---
+++ src/TodoStatusFilter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const TodoStatusFilter = () => {
+    return (
+        <div>
+            <input type={"button"} value={"all"}/>
+            <input type={"button"} value={"active"}/>
+            <input type={"button"} value={"done"}/>
+        </div>
+    )
+}
+
+export default TodoStatusFilter;
